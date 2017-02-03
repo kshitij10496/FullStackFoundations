094907b1fe1a3a1c5fb093e9b337d98f7d6df624
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-02-03 15:27:14 +0530

    creating database with SQLAlchemy: configuration

---
+++ database_setup.py
@@ -0,0 +1,13 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+######## at the end of the file ########
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
