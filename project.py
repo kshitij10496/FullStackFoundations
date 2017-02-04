50ce816d9f1d325e5257d5c58b2829ed7fa2c936
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-02-04 17:38:09 +0530

    adding database to Flask app

--- project.py
+++ project.py
@@ -1,11 +1,27 @@
 from flask import Flask
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Restaurant, MenuItem # importing classes
+
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db') 
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/hello')
 def hello():
-    return 'Hello World!'
+    items = session.query(MenuItem).all()
+    output = ''
+    for item in items:
+        output += item.name + '<br>'
+        output += item.price + '<br>'
+        output += item.description + '<br>'
+    return output
 
 if __name__ == '__main__':
     app.debug = True
