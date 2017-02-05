b03bb8d916635bd51cb4eb8eb7670c4fbb1cefce
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2017-02-05 10:03:59 +0530

    url quiz

--- project.py
+++ project.py
@@ -14,8 +14,9 @@ session = DBSession()
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 # Task 1: Create route for newMenuItem function here
